110308ab35a6d139b513f49812037073428c58e6
sethblocks <56612770+sethblocks@users.noreply.github.com>
2023-05-12 19:16:02 -0700

    updated

--- IF 2060.py
+++ IF 2060.py
@@ -1,18 +1,33 @@
+import os
 from diffusers import DiffusionPipeline
 from diffusers.utils import pt_to_pil
 import torch
+from transformers import T5EncoderModel
+stage_1 = DiffusionPipeline.from_pretrained("DeepFloyd/IF-I-XL-v1.0", variant="fp16", torch_dtype=torch.float16, )
 
-# stage 1
-stage_1 = DiffusionPipeline.from_pretrained("DeepFloyd/IF-I-XL-v1.0", variant="fp16", torch_dtype=torch.float16)
 #stage_1.enable_xformers_memory_efficient_attention()  # remove line if torch.__version__ >= 2.0.0
 stage_1.enable_model_cpu_offload()
 stage_1.enable_attention_slicing(1)
 
-prompt= "Circuit board with text that says Floyd IF"
+prompt= "circuit board with text saying RTX 2060"
 prompt_embeds, negative_embeds = stage_1.encode_prompt(prompt)
-image = stage_1(prompt_embeds=prompt_embeds, negative_prompt_embeds= negative_embeds, num_inference_steps=15)
+
+
+image = stage_1(prompt_embeds=prompt_embeds, negative_prompt_embeds= negative_embeds, num_inference_steps=15) #let's go
 imagepng=image.images[0]
-imagepng.save("RTX 2060.png")
+imagepng.save('img.png')
+
+stage_2 = DiffusionPipeline.from_pretrained(
+    "DeepFloyd/IF-II-L-v1.0", text_encoder=None, variant="fp16", torch_dtype=torch.float16
+)
+stage_2.enable_model_cpu_offload()
+stage_2.enable_attention_slicing(1)
+images2 = stage_2(
+    image=imagepng,
+    prompt_embeds=prompt_embeds,
+    negative_prompt_embeds=negative_embeds,
+    output_type="pt",
+).images[0]
 
 safety_modules = {
     "feature_extractor": stage_1.feature_extractor,
@@ -26,7 +41,6 @@ stage_3 = DiffusionPipeline.from_pretrained(
 stage_3.enable_model_cpu_offload()
 stage_3.enable_attention_slicing(1)
 
-stage_3 = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-x4-upscaler", **safety_modules, torch_dtype=torch.float16)
 
-HDimg = stage_3(prompt=prompt, image=imagepng).images[0]
-HDimg.save("RTXHD.png")
+HDimg = stage_3(prompt=prompt, image=images2).images[0]
+HDimg.save('HD.png')
